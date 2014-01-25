068c26b28181612871b672936fa9f221ca31d28d
Evan Li <evanyeejinli@gmail.com>
2014-01-25 01:16:05 -0500

    Implemented fetch.php functions

--- resources/fetch.php
+++ resources/fetch.php
@@ -5,13 +5,39 @@ require('database.php');
 /**
  * find availabilities around given coord
  */
-function getPoints($user, $num, $long, $lat) {
-	
+function getPoints($user, $long, $lat) {
+	$dLong = 1;
+	$dLat = 1;
+
+	return getPointsInBox($user, $long - $dLong, $long + $dLong,
+		$lat - $dLat, $lat + $dLat);
 }
 
 /**
  * find availabilities within bounding box
  */
 function getPointsInBox($user, $minLong, $maxLong, $minLat, $maxLat) {
+	// sanitization
+	$user = mysql_real_escape_string(substr($user, 0, 32));
+
+	$result = mysql_query("SELECT * FROM people WHERE
+		latitude > '{$minLat}' AND latitude < '{$maxLat}' AND
+		longitude > '{$minLong}' AND longitude < '{$maxLong}';");
 	
+	/*if (mysql_num_rows($result) > 200) {
+		$result = mysql_query("SELECT * ");
+	}*/
+
+	$output = array();
+	
+	// get row in db
+	while ($row = mysql_fetch_assoc($result)) {
+		$output[] = (object) array(
+			'user' => $row['user'],
+			'blurb' => $row['blurb'],
+			'longitude' => doubleval($row['longitude']),
+			'latitude' => doubleval($row['latitude']),
+			'createTime' => intval($row['createTime']));
+	}
+	return json_encode($output);
 }
