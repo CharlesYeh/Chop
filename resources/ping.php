78c6ad15a221ab51b3ee10c75e8b833604574ed4
Evan Li <evanyeejinli@gmail.com>
2014-01-25 00:10:01 -0500

    Implemented pingOn and pingOff functions.

--- resources/ping.php
+++ resources/ping.php
@@ -7,15 +7,16 @@ require('database.php');
  */
 function pingOn($user, $blurb, $longitude, $latitude) {
 	// sanitization
-	$user 	= mysql_real_escape_string(substr($blurb, 0, 32));
-	$value	= intval($value);
-	$blurb	= substr($blurb, 0, min(strlen($blurb), 140));
-	$longitude	= doubleval($longitude);
-	$latitude		= doubleval($latitude);
+	$user = mysql_real_escape_string(substr($user, 0, 32));
+	$blurb = mysql_real_escape_string(substr($blurb, 0, min(strlen($blurb), 140)));
+	$longitude = doubleval($longitude);
+	$latitude = doubleval($latitude);
+	$createTime = time();
 	
 	// i'm available!!
-	$result = mysql_query("SELECT * ");
-	if (mysql_num_rows($result) > 200) {
+	$result = mysql_query("INSERT INTO people VALUES ('{$user}', '{$blurb}',
+		'{$latitude}', '{$longitude}', '{$createTime}')");
+	/*if (mysql_num_rows($result) > 200) {
 		$result = mysql_query("SELECT * ");
 	}
 	
@@ -26,9 +27,11 @@ function pingOn($user, $blurb, $longitude, $latitude) {
 		$row['longitude'];
 		$row['longitude'];
 		$row['latitude'];
-	}
+	}*/
 }
 
 function pingOff($user) {
-	
+	$user = mysql_real_escape_string(substr($user, 0, 32));
+
+	$result = mysql_query("DELETE FROM people WHERE user = '{$user}'");
 }
