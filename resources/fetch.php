02a808ae79765cd1688d9b912e34ece7020df11e
Charles Yeh <charles_yeh@brown.edu>
2014-01-27 01:01:26 -0500

    updated graphics

--- resources/fetch.php
+++ resources/fetch.php
@@ -22,7 +22,7 @@ function getPointsInBox($user, $minLong, $maxLong, $minLat, $maxLat) {
 
 	// 1 minute cutoff
 	$cutoff = time() - 60;
-	$result = mysql_query("SELECT * FROM people WHERE user <> '{$user}' AND
+	$result = mysql_query("SELECT * FROM people WHERE
 		latitude > {$minLat} AND latitude < {$maxLat} AND
 		longitude > {$minLong} AND longitude < {$maxLong} AND createTime > {$cutoff};");
 	
@@ -32,14 +32,28 @@ function getPointsInBox($user, $minLong, $maxLong, $minLat, $maxLat) {
 
 	$output = array();
 	
+	$showSelf = (mysql_num_rows($result) == 1);
+	
 	// get row in db
 	while ($row = mysql_fetch_assoc($result)) {
-		$output[] = (object) array(
-			'user' => $row['user'],
-			'blurb' => $row['blurb'],
-			'longitude' => doubleval($row['longitude']),
-			'latitude' => doubleval($row['latitude']),
-			'createTime' => intval($row['createTime']));
+		if ($row['user'] == $user) {
+			if ($showSelf) {
+				$output[] = (object) array(
+					'user' => $row['user'],
+					'blurb' => '[no people are online :( this is you]',
+					'longitude' => doubleval($row['longitude']),
+					'latitude' => doubleval($row['latitude']),
+					'createTime' => intval($row['createTime']));
+			}
+		}
+		else {
+			$output[] = (object) array(
+				'user' => $row['user'],
+				'blurb' => $row['blurb'],
+				'longitude' => doubleval($row['longitude']),
+				'latitude' => doubleval($row['latitude']),
+				'createTime' => intval($row['createTime']));
+		}
 	}
 	
 	return json_encode($output);
